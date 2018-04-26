07fbab3a458faa486715fab346ae3b0018ef5879
thiagomg95 <37165261+thiagomg95@users.noreply.github.com>
2018-04-26 12:19:52 -0300

    Update starwars.js

--- starwars.js
+++ starwars.js
@@ -3,3 +3,45 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+
+// Seu javascript aqui :)
+// Use a Star Wars API: https://swapi.co/
+// para carregar:
+//  - A lista de filmes
+//  - A introdução de cada filme, quando ele for clicado
+
+
+
+$.ajax(
+  {
+    url: 'https://swapi.co/api/films/',
+    method: 'GET',
+    success: function(resposta)
+    {
+      resposta.results.sort(function(a,b)
+    {
+      if(a.episode_id<b.episode_id)
+      {
+        return -1;
+      }
+      else if(a.episode_id == b.episode_id)
+      {
+        return 0;
+      }
+      else
+      {
+        return 1;
+      }
+    })
+
+    let filmes = resposta.results;
+    let escolha = $("#movies > ul");
+    for(var film of filmes)
+    {
+      $("escolha").append($("li"));
+    }
+
+      console.log(resposta.results);
+    }
+  }
+);
