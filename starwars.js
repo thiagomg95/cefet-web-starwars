5a1119f9bf9352d5073f7e55145e45f6fcce86fc
thiagomg95 <37165261+thiagomg95@users.noreply.github.com>
2018-04-26 23:31:01 -0300

    Update starwars.js

--- starwars.js
+++ starwars.js
@@ -10,38 +10,54 @@
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
 
-
+let textosDeAbertura = [];
 
 $.ajax(
   {
     url: 'https://swapi.co/api/films/',
     method: 'GET',
-    success: function(resposta)
-    {
-      resposta.results.sort(function(a,b)
-    {
-      if(a.episode_id<b.episode_id)
-      {
-        return -1;
-      }
-      else if(a.episode_id == b.episode_id)
-      {
-        return 0;
-      }
-      else
-      {
-        return 1;
-      }
-    })
+    success: function(resposta) 
+        {
+            resposta.results.sort(function(a,b)
+                {
+                  if(a.episode_id<b.episode_id)
+                  {
+                    return -1;
+                  }
+                  else if(a.episode_id == b.episode_id)
+                  {
+                    return 0;
+                  }
+                  else
+                  {
+                    return 1;
+                  }
+                }
+            );
 
     let filmes = resposta.results;
-    let escolha = $("#movies > ul");
-    for(var film of filmes)
+
+    for (var filme of filmes)
     {
-      $("escolha").append($("li"));
+        $("ul").append  ("<li id=\'episode"+ filme.episode_id + "\' data-episode-url=\"http://swapi.co/api/films/"+filme.episode_id +"/\">Episode "+filme.episode_id+ ": "+ filme.title + "</li> ");
+        textosDeAbertura[filme.episode_id] = "Episode " + filme.episode_id + "\n" + filme.title.toUpperCase() + "\n\n" + filme.opening_crawl;
     }
 
-      console.log(resposta.results);
+    if (sessionStorage.getItem('historico') != null)
+      {
+        $("pre").html(textosDeAbertura[ sessionStorage.getItem('historico') ]);
+      }
+        
     }
   }
+);
+
+$('#movies').on('click', 'li', function(e) 
+    {
+        let string = this.getAttribute("id");
+        string = string.substring(7);
+        $("pre").html(textosDeAbertura[string]);
+
+        sessionStorage.setItem('historico', string);
+    }
 );
